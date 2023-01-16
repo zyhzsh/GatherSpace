a5776fb7ebd2cfa08f2b6110ad53494c85d177a3
Shenghang Zhu <67240191+zyhzsh@users.noreply.github.com>
2023-01-16 22:17:30 +0100

    init project

---
+++ README.md
@@ -0,0 +1,30 @@
+## ![Logo](./doc/static/img/logo.png)
+
+## Hosting your events, share your joy
+
+### :pushpin: Guide
+
+- [Documentation]()
+- [Preview]()
+
+### :hammer_and_wrench: Stack
+
+<div align=center>
+  <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nestjs/nestjs-plain.svg" title="Csharp" alt="Nest.Js" width="60" height="60"/>&nbsp;
+  <img src="https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/react/react-original-wordmark.svg" title="React" alt="React" width="60" height="60"/>&nbsp;
+  <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-plain.svg" title="TypeScript" alt="React" width="60" height="60"/>&nbsp;
+  <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/materialui/materialui-plain.svg" title="MUI" alt="JavaScript" width="60" height="60"/>&nbsp;
+  <img src="https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/javascript/javascript-original.svg" title="JavaScript" alt="JavaScript" width="60" height="60"/>&nbsp;
+  <img src="https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/docker/docker-original.svg" title="Docker" alt="Docker" width="60" height="60"/>
+</div>
+<br/>
+
+### :bulb: Maintainers
+
+<table align=center>
+  <tbody>
+    <tr>
+    <td align="center"><a><img src="https://avatars.githubusercontent.com/u/67240191?v=4" width="100px;" alt="Shenghang Zhu"/><br><sub><b>Shenghang Zhu</b></a></td>
+    </tr>
+  </tbody>
+</table>
---
+++ doc/docusaurus.config.js
@@ -0,0 +1,76 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Documentation',
+  tagline: 'Documentation',
+  url: 'https://www.google.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en','zh-CN'],
+  },
+  themes: [
+    '@easyops-cn/docusaurus-search-local',
+  ],
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: '',
+        logo: {
+          alt: 'Logo',
+          className: "Logo",
+          src: 'img/logo.svg',
+        },
+        items: [
+          {
+            href: 'https://google.com',
+            // label: 'GitHub',
+            className: "header-github-link",
+            "aria-label": "GitHub repository",
+            position: 'right',
+          },
+          // {
+          //   href: 'https://github.com/facebook/docusaurus',
+          //   label: 'GitHub',
+          //   position: 'right',
+          // },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Gather Space`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
